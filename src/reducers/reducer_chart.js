f51cd1061bf0448c039ae01a90c7d89e178b1cbe
ottovbismarck <fritzj96@gmail.com>
2018-08-17 22:21:10 -0700

    Redux Reducer work + added redux library that I forgot to.

---
+++ src/reducers/reducer_chart.js
@@ -0,0 +1,9 @@
+import { FETCH_TICKER_DATA } from '../actions/index';
+
+export default function(state = [], action) {
+    switch(action.type) {
+        case FETCH_TICKER_DATA:
+            return [action.payload.data, ...state]
+    }
+    return state;
+}
