6dc2dab29fe1e07102576fb5e426d071dcc35f39
ottovbismarck <fritzj96@gmail.com>
2018-08-19 16:15:02 -0700

    Starting on Layout in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,12 @@ import SearchBar from '../containers/SearchBar';
 class App extends Component {
   render() {
     return (
-      <div>
-        <SearchBar/>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <SearchBar/>
+          </div>
+        </div>
       </div>
     );
   }
